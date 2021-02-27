8d04a99af9965338deb4482c33e9892bbed437f1
fauzitoushiro <fauzitoushiro@gmail.com>
2021-02-27 12:18:24 +0800

    model uses

---
+++ app/src/main/java/com/praktikunandroid7live/model/User.kt
@@ -0,0 +1,23 @@
+package com.praktikunandroid7live.model
+
+data class User(
+        val page: Int,
+        val per_page: Int,
+        val total: Int,
+        val total_pages: Int,
+        val data : List<UserData>
+)
+
+data class UserData(
+        @field:Json(name = "@id")
+        val id: Int,
+        @field:Json(name = "@email")
+        val email: String,
+        @field:Json(name = "@first_name")
+        val first_name: String,
+        @field:Json(name = "@last_name")
+        val last_name: String,
+        @field:Json(name = "@avatar")
+        val avatar: String
+)
+}
