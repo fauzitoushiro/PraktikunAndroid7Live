e2da2b69e7b4b4bc8b6699767c5ada89b1618f8e
fauzitoushiro <fauzitoushiro@gmail.com>
2021-03-13 22:54:46 +0800

    Praktikum Android Semester 7: Part 5

---
+++ app/src/main/java/com/praktikunandroid7live/JenisbarangPostActivity.kt
@@ -0,0 +1,11 @@
+package com.praktikunandroid7live
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class JenisbarangPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_jenisbarang_post)
+    }
+}
--- app/src/main/java/com/praktikunandroid7live/ui/jenisbarang/JenisbarangViewModel.kt
+++ app/src/main/java/com/praktikunandroid7live/ui/jenisbarang/JenisbarangViewModel.kt
@@ -5,11 +5,15 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.praktikunandroid7live.model.Jenisbarang
+import com.praktikunandroid7live.model.JenisbarangData
+import com.praktikunandroid7live.model.JenisbarangResponse
 import com.praktikunandroid7live.network.Api
 import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class JenisbarangViewModel : ViewModel() {
     private val _response = MutableLiveData<Jenisbarang>()
+    val createResponse = MutableLiveData<Response<JenisbarangResponse>>()
 
     val response: LiveData<Jenisbarang>
         get() = _response
@@ -26,6 +30,13 @@ class JenisbarangViewModel : ViewModel() {
             } catch (e: Exception) {
                 _response.value = null
             }
+        }
+    }
+
+    fun create(jenisbarangData: JenisbarangData) {
+        viewModelScope.launch {
+            val response = Api.retrofitService.create(jenisbarangData)
+            createResponse.value = response
         }
     }
 }
