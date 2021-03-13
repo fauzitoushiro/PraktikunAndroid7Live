b8617a18dddd39ed74216d45f94c9002ba210e5e
fauzitoushiro <fauzitoushiro@gmail.com>
2021-03-13 11:44:28 +0800

    Praktikum Android Semester 7: Part 4

--- app/src/main/java/com/praktikunandroid7live/network/ApiService.kt
+++ app/src/main/java/com/praktikunandroid7live/network/ApiService.kt
@@ -1,5 +1,6 @@
 package com.praktikunandroid7live.network
 
+import com.praktikunandroid7live.model.Jenisbarang
 import com.praktikunandroid7live.model.User
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
@@ -7,21 +8,24 @@ import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
-private const val BASE_URL = "https://reqres.in/api/"
+private const val BASE_URL = "http://10.0.2.2/praktikum-penjualan-api-starter/api/"
 
 private val moshi = Moshi.Builder()
-        .add(KotlinJsonAdapterFactory())
-        .build()
+    .add(KotlinJsonAdapterFactory())
+    .build()
 
 private val retrofit = Retrofit.Builder()
-        .addConverterFactory(MoshiConverterFactory.create(moshi))
-        .baseUrl(BASE_URL)
-        .build()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
 
 
 interface ApiService {
     @GET("users?page=1")
     suspend fun getUsers(): User
+
+    @GET("jenisbarang/read.php")
+    suspend fun getJenisbarang(): Jenisbarang
 }
 
 object Api {
